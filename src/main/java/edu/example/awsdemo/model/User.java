d7e3c984b6057a1afc54c662046e2cf940335dd6
wnsgho <darkblueuu@gmail.com>
2024-11-11 16:47:25 +0900

    1a

---
+++ src/main/java/edu/example/awsdemo/model/User.java
@@ -0,0 +1,17 @@
+package edu.example.awsdemo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+}
---
+++ src/main/java/edu/example/awsdemo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package edu.example.awsdemo.repository;
+
+import edu.example.awsdemo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
